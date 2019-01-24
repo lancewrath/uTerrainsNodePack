b48777fc52ac1bce653d5a05ec0d552ce7a80e21
lancewrath <landon.g.ritchie@gmail.com>
2019-01-23 19:14:38 -0800

    Initial Upload

---
+++ Assets/uTerrains/Scripts/Nodes/Filters/TurbulenceFilter.cs
@@ -0,0 +1,47 @@
+﻿using UltimateTerrains;
+using LibNoise;
+using LibNoise.Primitive;
+
+public class TurbulenceFilter : FilterNode
+{
+
+
+    private readonly double frequency;
+    private readonly double power;
+    private readonly int seed;
+    private readonly ImprovedPerlin distort;
+    private readonly CallableNode input;
+
+    private const double X0 = (12414.0 / 65536.0);
+    private const double Y0 = (65124.0 / 65536.0);
+    private const double Z0 = (31337.0 / 65536.0);
+    private const double X1 = (26519.0 / 65536.0);
+    private const double Y1 = (18128.0 / 65536.0);
+    private const double Z1 = (60493.0 / 65536.0);
+    private const double X2 = (53820.0 / 65536.0);
+    private const double Y2 = (11213.0 / 65536.0);
+    private const double Z2 = (44845.0 / 65536.0);
+
+    public TurbulenceFilter(CallableNode input, CallableNode maskInput, double intensity, double frequency, double power, int seed) : base(input, maskInput, intensity)
+    {
+        this.input = input;
+        this.frequency = frequency;
+        this.power = power;
+        this.seed = seed;
+        this.distort = new ImprovedPerlin(this.seed, NoiseQuality.Standard);
+    }
+
+
+
+    protected override double ExecuteFilter(double x, double y, double z, CallableNode flow, double inputValue)
+    {
+
+        var xd = x + (distort.GetValue((x*frequency) + X0, (y * frequency) + Y0, (z * frequency) + Z0) * power);
+        var yd = y + (distort.GetValue((x * frequency) + X1, (y * frequency) + Y1, (z * frequency) + Z1) * power);
+        var zd = z + (distort.GetValue((x * frequency) + X2, (y * frequency) + Y2, (z * frequency) + Z2) * power);
+        return flow.Call(input, xd, yd, zd);
+
+    }
+
+
+}
---
+++ Assets/uTerrains/Scripts/Nodes/Filters/TurbulenceFilterSerializable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using UltimateTerrains;
+using UltimateTerrainsEditor;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+
+#endif
+[PrettyTypeName("Turbulence")]
+[Serializable]
+public class TurbulenceFilterSerializable : FilterNodeSerializable
+{
+    public override string Title
+    {
+        get { return "Turbulence"; }
+    }
+
+
+
+    [SerializeField] private float frequency;
+    [SerializeField] private float power;
+    [SerializeField] private int seed;
+
+    public override void OnEditorGUI(UltimateTerrain uTerrain)
+    {
+#if UNITY_EDITOR
+        EditorGUIUtility.labelWidth = 60;
+        frequency = EditorGUILayout.FloatField("Frequency", frequency);
+        power = EditorGUILayout.FloatField("Power", power);
+        seed = EditorGUILayout.IntField("Seed", seed);
+        base.OnEditorGUI(uTerrain);
+        EditorGUIUtility.labelWidth = 0;
+#endif
+    }
+
+    public override IGeneratorNode CreateModule(UltimateTerrain uTerrain, List<CallableNode> inputs)
+    {
+        return new TurbulenceFilter(inputs[0], inputs[1], Intensity, frequency, power, seed);
+    }
+}
