c376076904b02727c1f129dc09a2aebc4a52768c
lancewrath <landon.g.ritchie@gmail.com>
2019-01-23 19:41:19 -0800

    Added Billow & Utils

---
+++ Assets/uTerrains/Scripts/Nodes/Primitives/2D/BillowPrimitive.cs
@@ -0,0 +1,57 @@
+﻿using LibNoise;
+using LibNoise.Primitive;
+using UltimateTerrains;
+using UltimateTerrains.Utils;
+
+public class BillowPrimitive : Primitive2DNode
+{
+    private readonly double frequency = 1.0;
+    private readonly double lacunarity = 2.0;
+    private readonly NoiseQuality quality = NoiseQuality.Standard;
+    private readonly int octaveCount = 6;
+    private readonly double persistence = 0.5;
+    private readonly int seed;
+
+    public BillowPrimitive(double frequency, double lacunarity, int seed, int octaveCount, double persistence, NoiseQuality quality)
+    {
+        this.frequency = frequency;
+        this.lacunarity = lacunarity;
+        this.octaveCount = octaveCount;
+        this.persistence = persistence;
+        this.quality = quality;
+        //perlin = new ImprovedPerlin(seed, quality);
+    }
+
+    public override double Execute(double x, double y, double z, CallableNode flow)
+    {
+
+        var value = 0.0;
+        var amplitude = 1.0;
+
+        x *= frequency;
+        y *= frequency;
+        z *= frequency;
+        for (var i = 0; i < octaveCount; i++)
+        {
+            var nx = Utils.MakeInt32Range(x);
+            var ny = Utils.MakeInt32Range(y);
+            var nz = Utils.MakeInt32Range(z);
+            var seed = (this.seed + i) & 0xffffffff;
+            var signal = Utils.GradientCoherentNoise3D(nx, ny, nz, seed, quality);
+            signal = 2.0 * System.Math.Abs(signal) - 1.0;
+            value += signal * amplitude;
+            x *= lacunarity;
+            y *= lacunarity;
+            z *= lacunarity;
+            amplitude *= persistence;
+        }
+        return value + 0.5;
+
+
+
+    }
+
+    
+
+
+}
---
+++ Assets/uTerrains/Scripts/Nodes/Primitives/2D/BillowPrimitiveSerializable.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using LibNoise;
+using UltimateTerrains;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+
+#endif
+
+[PrettyTypeName("Billow 2D")]
+[Serializable]
+public class BillowPrimitiveSerializable : Primitive2DNodeSerializable
+{
+
+    public override string Title
+    {
+        get { return "Billow 2D"; }
+    }
+
+    // Useful properties for the module
+    [SerializeField] private float frequency = 1f / 90f;
+    [SerializeField] private float lacunarity = 2.0f;
+    [SerializeField] private int octaveCount = 6;
+    [SerializeField] private int seed;
+    [SerializeField] private float persistence = 2.0f;
+    [SerializeField] private NoiseQuality quality = NoiseQuality.Standard;
+
+    public override void OnEditorGUI(UltimateTerrain uTerrain)
+    {
+#if UNITY_EDITOR
+        frequency = EditorGUILayout.FloatField("Frequency:", frequency);
+        lacunarity = EditorGUILayout.FloatField("Lacunarity:", lacunarity);
+        persistence = EditorGUILayout.FloatField("Persistance:", persistence);
+        octaveCount = EditorGUILayout.IntField("Octaves:", octaveCount);
+        seed = EditorGUILayout.IntField("Seed:", seed);
+        quality = (NoiseQuality)EditorGUILayout.EnumPopup("Quality:", quality);
+#endif
+    }
+
+    public override IGeneratorNode CreateModule(UltimateTerrain uTerrain, List<CallableNode> inputs)
+    {
+        return new BillowPrimitive(frequency, lacunarity, seed, octaveCount, persistence, quality);
+    }
+}
