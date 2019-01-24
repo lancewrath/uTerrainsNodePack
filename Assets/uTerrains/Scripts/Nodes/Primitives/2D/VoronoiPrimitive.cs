0e3150a0507e9fc4b5ab2230e5d2a6c2e73911d2
lancewrath <landon.g.ritchie@gmail.com>
2019-01-24 10:51:36 -0800

    Added Voronoi

---
+++ Assets/uTerrains/Scripts/Nodes/Primitives/2D/VoronoiPrimitive.cs
@@ -0,0 +1,80 @@
+﻿using LibNoise;
+using LibNoise.Primitive;
+using UltimateTerrains;
+using UltimateTerrains.Utils;
+using System;
+
+public class VoronoiPrimitive : Primitive2DNode
+{
+
+    private readonly double displacement = 1.0;
+    private readonly double frequency = 1.0;
+    private readonly int seed;
+    private readonly bool distance;
+
+    private readonly NoiseQuality quality;
+
+    public VoronoiPrimitive(double frequency, bool distance, double displacement, int seed, NoiseQuality quality)
+    {
+        this.frequency = frequency;
+        this.displacement = displacement;
+        this.distance = distance;
+        this.quality = quality;
+        //perlin = new ImprovedPerlin(seed, quality);
+    }
+
+    public override double Execute(double x, double y, double z, CallableNode flow)
+    {
+        x *= frequency;
+        y *= frequency;
+        z *= frequency;
+        var xi = (x > 0.0 ? (int)x : (int)x - 1);
+        var iy = (y > 0.0 ? (int)y : (int)y - 1);
+        var iz = (z > 0.0 ? (int)z : (int)z - 1);
+        var md = 2147483647.0;
+        double xc = 0;
+        double yc = 0;
+        double zc = 0;
+        for (var zcu = iz - 2; zcu <= iz + 2; zcu++)
+        {
+            //for (var ycu = iy - 2; ycu <= iy + 2; ycu++)
+            //{
+                for (var xcu = xi - 2; xcu <= xi + 2; xcu++)
+                {
+                    var xp = xcu + uUtils.ValueNoise3D(xcu, 0, zcu, seed);
+                    //var yp = ycu + uUtils.ValueNoise3D(xcu, ycu, zcu, seed + 1);
+                    var zp = zcu + uUtils.ValueNoise3D(xcu, 0, zcu, seed + 2);
+                    var xd = xp - x;
+                    //var yd = yp - y;
+                    var zd = zp - z;
+                    var d = xd * xd + zd * zd;
+                    //var d = xd * xd + yd * yd + zd * zd;
+                    if (d < md)
+                    {
+                        md = d;
+                        xc = xp;
+                        //yc = yp;
+                        zc = zp;
+                    }
+                }
+           // }
+        }
+        double v;
+        if (distance)
+        {
+            var xd = xc - x;
+            //var yd = yc - y;
+            var zd = zc - z;
+            v = (Math.Sqrt(xd * xd + zd * zd)) * uUtils.Sqrt3 - 1.0;
+            //v = (Math.Sqrt(xd * xd + yd * yd + zd * zd)) * uUtils.Sqrt3 - 1.0;
+        }
+        else
+        {
+            v = 0.0;
+        }
+        return (v + (displacement * uUtils.ValueNoise3D((int)(Math.Floor(xc)), 0,
+            (int)(Math.Floor(zc)), seed)));
+    }
+
+
+}
---
+++ Assets/uTerrains/Scripts/Nodes/Primitives/2D/VoronoiPrimitiveSerializable.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using LibNoise;
+using UltimateTerrains;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+
+#endif
+
+[PrettyTypeName("Voronoi 2D")]
+[Serializable]
+public class VoronoiPrimitiveSerializable : Primitive2DNodeSerializable
+{
+    public override string Title
+    {
+        get { return "Voronoi 2D"; }
+    }
+
+    [SerializeField] private float displacement = 0.5f;
+    [SerializeField] private float frequency = 0.008f;
+    [SerializeField] private int seed;
+    [SerializeField] private bool distance = true;
+   
+    [SerializeField] private NoiseQuality quality;
+    // Use this for initialization
+    public override void OnEditorGUI(UltimateTerrain uTerrain)
+    {
+#if UNITY_EDITOR
+        frequency = EditorGUILayout.FloatField("Frequency:", frequency);
+        displacement = EditorGUILayout.FloatField("Displacement:", displacement);
+        
+        seed = EditorGUILayout.IntField("Seed:", seed);
+        distance = EditorGUILayout.Toggle("Use Distance", distance);
+        quality = (NoiseQuality)EditorGUILayout.EnumPopup("Quality:", quality);
+#endif
+    }
+
+    public override IGeneratorNode CreateModule(UltimateTerrain uTerrain, List<CallableNode> inputs)
+    {
+        return new VoronoiPrimitive(frequency,distance,displacement,seed,quality);
+    }
+}
